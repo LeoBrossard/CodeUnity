818a7250a7bb33ea62955aa63e9372bf04e1268e
LeoBrossard <74302619+LeoBrossard@users.noreply.github.com>
2020-11-11 14:04:13 +0100

    Add files via upload

---
+++ Player_Mouvement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Player_Mouvement : MonoBehaviour
+{
+
+    public float speed;             //La vitesse de deplacement
+    public float raycastDistance;   //La portee de detection des murs
+
+    RaycastHit2D hitRight;          //Le rayon pour detecter les murs
+    float inputX;                   //Input sur les x Joueur
+    float inputY;                   //Input sur les y du Joueur
+
+    void Update()
+    {
+
+        inputX = Input.GetAxis("Horizontal");
+        inputY = Input.GetAxis("Vertical");
+
+
+        if(!Physics2D.Raycast(transform.position, Vector3.right, raycastDistance, LayerMask.NameToLayer("Wall")) && inputX > 0)
+        {
+
+            Move(Vector3.right);
+
+        }
+        if (!Physics2D.Raycast(transform.position, Vector3.left, raycastDistance, LayerMask.NameToLayer("Wall")) && inputX < 0)
+        {
+
+            Move(Vector3.left);
+
+        }
+        if (!Physics2D.Raycast(transform.position, Vector3.up, raycastDistance, LayerMask.NameToLayer("Wall")) && inputY > 0)
+        {
+
+            Move(Vector3.up);
+
+        }
+        if (!Physics2D.Raycast(transform.position, Vector3.down, raycastDistance, LayerMask.NameToLayer("Wall")) && inputY < 0)
+        {
+
+            Move(Vector3.down);
+
+        }
+
+
+    }
+
+    private void Move(Vector3 targetLocation)
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + targetLocation, speed * Time.deltaTime);
+
+    }
+
+}
---
+++ ProceduralDungeon_Manager.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+
+public class ProceduralDungeon_Manager : MonoBehaviour
+{
+
+    public Dungeon[] pieces;
+
+    public Dungeon entree;
+    public Dungeon sortie;
+
+    public int dungeonLength;
+    public Vector3 dungeonOffSet;
+
+    Vector3 piecePosition;
+    Vector3[] existingPiecePosition;
+    bool error = false;
+
+
+    void Start()
+    {
+        Reset();
+    }
+
+    void Generate()
+    {
+        existingPiecePosition = new Vector3[dungeonLength];
+
+        Instantiate(entree, dungeonOffSet, Quaternion.identity);
+
+        existingPiecePosition[0] = dungeonOffSet;
+
+        for(int i = 1; i < dungeonLength - 1; i++){
+
+            RandomPiecePosition();
+
+            existingPiecePosition[i] = piecePosition;
+
+            Instantiate(pieces[(int)Random.Range(0, pieces.Length)], dungeonOffSet + piecePosition, Quaternion.identity);
+
+        }
+
+        RandomPiecePosition();
+
+        Instantiate(sortie, dungeonOffSet, Quaternion.identity);
+
+        if (error)
+        {
+            Reset();
+        }
+
+    }
+
+    void Reset()
+    {
+
+        foreach(GameObject _piece in GameObject.FindGameObjectsWithTag("piece")){
+
+            Destroy(_piece);
+
+        }
+
+        existingPiecePosition = new Vector3[dungeonLength];
+
+        error = false;
+
+        Generate();
+
+    }
+
+    void RandomPiecePosition()
+    {
+
+        if (Random.value < 0.25f && AvailablePlacement(new Vector3(1, 0)))
+        {
+            piecePosition += new Vector3(1, 0);
+        }
+        else if(0.5f > Random.value && Random.value > 0.25f && AvailablePlacement(new Vector3(-1, 0)))
+        {
+            piecePosition += new Vector3(-1, 0);
+        }
+        else if (0.5f < Random.value && Random.value < 0.75f && AvailablePlacement(new Vector3(0, 1)))
+        {
+            piecePosition += new Vector3(0, 1);
+        }
+        else if(AvailablePlacement(new Vector3(0, -1)))
+        {
+            piecePosition += new Vector3(0, -1);
+        }
+        else
+        {
+            error = true;
+        }
+    }
+
+    bool AvailablePlacement(Vector3 placement)
+    {
+
+        for (int i = 0; i < existingPiecePosition.Length; i++)
+        {
+
+            if (existingPiecePosition[i] == placement)
+            {
+                return false;
+            }
+
+        }
+
+        return true;
+
+    }
+
+
+}
